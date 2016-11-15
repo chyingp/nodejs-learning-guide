0562adfb88ac6abb4596f09f51fd92ad603e2b50
casperchen(陈映平) <chyingp@gmail.com>
2016-11-16 01:16:43 +0800

    修改post例子

--- examples/2016.11.08-node-http/client/requestPost.js
+++ examples/2016.11.08-node-http/client/requestPost.js
@@ -1,21 +1,39 @@
 var http = require('http');
-var options = {
-    protocol: 'http:',
-    hostname: 'id.qq.com',
-    port: '80',
-    path: '/',
-    method: 'GET'
+var querystring = require('querystring');
+
+var createClientPostRequest = function(){
+    var options = {
+        method: 'POST',
+        protocol: 'http:',
+        hostname: '127.0.0.1',
+        port: '3000',
+        path: '/post',
+        headers: {
+            "connection": "keep-alive",
+            "content-type": "application/x-www-form-urlencoded"
+        }    
+    };
+
+    // 发送给服务端的数据
+    var postBody = {
+        nick: 'chyingp'
+    };
+
+    // 创建客户端请求
+    var client = http.request(options, function(res){
+        // 最终输出：Server got client data: nick=chyingp
+        res.pipe(process.stdout);  
+    });
+
+    // 发送的报文主体，记得先用 querystring.stringify() 处理下
+    client.write( querystring.stringify(postBody) );
+    client.end();
 };
 
-var client = http.request(options, function(res){
-    var data = '';
-    res.setEncoding('utf8');
-    res.on('data', function(chunk){
-        data += chunk;
-    });
-    res.on('end', function(){
-        console.log(data);
-    });
+// 服务端程序，只是负责回传客户端数据
+var server = http.createServer(function(req, res){
+    res.write('Server got client data: ');
+    req.pipe(res);
 });
 
-client.end();
+server.listen(3000, createClientPostRequest);

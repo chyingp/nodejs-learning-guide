9f42b48d7c04ffeed956974cdc51f46bcf66e2de
casperchen(陈映平) <chyingp@gmail.com>
2016-12-01 12:21:53 +0800

    基础例子

---
+++ examples/2016.12.01-events/basic.js
@@ -0,0 +1,15 @@
+var EventEmitter = require('events');
+
+class Man extends EventEmitter {}
+
+var man = new Man();
+
+man.on('wakeup', function(){
+    console.log('man has woken up');
+});
+
+man.on('wakeup', function(){
+    console.log('man has woken up again');
+});
+
+man.emit('wakeup');

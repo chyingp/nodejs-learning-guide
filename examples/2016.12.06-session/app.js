edf8fa14af27da4458e358b2461c0821715df812
casperchen(陈映平) <chyingp@gmail.com>
2016-12-06 23:07:09 +0800

    完善session例子

--- examples/2016.12.06-session/app.js
+++ examples/2016.12.06-session/app.js
@@ -5,8 +5,14 @@ var FileStore = require('session-file-store')(session);
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
-var USERNAME = 'chyingp';
-var PASSWORD = '123456';
+var identityKey = 'skey';
+var users = require('./users').items;
+
+var findUser = function(name, password){
+	return users.find(function(item){
+		return item.name === name && item.password === password;
+	});
+};
 
 app.set('views', 'views/');
 app.set('view engine', 'ejs');
@@ -18,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(session({
-	name: 'skey',
+	name: identityKey,
 	secret: 'chyingp',
 	store: new FileStore(),
 	saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
@@ -30,47 +36,48 @@ app.use(session({
 
 app.get('/', function(req, res, next){
 	var sess = req.session;
-	var isLogined = !!sess.name;
-
-	console.log( sess.name );
+	var loginUser = sess.loginUser;
+	var isLogined = !!loginUser;
 
 	res.render('index', {
 		isLogined: isLogined,
-		name: sess.name
+		name: loginUser || ''
 	});
 });
 
 app.post('/login', function(req, res, next){
 	
-	var name = USERNAME;
-	var password = PASSWORD;
-	
 	var sess = req.session;
+	var user = findUser(req.body.name, req.body.password);
 
-	if(req.body.name === name && req.body.password === password){
+	if(user){
 		req.session.regenerate(function(err) {
 			if(err){
 				return res.json({ret_code: 2, ret_msg: '登录失败'});				
 			}
-			req.session.name = name;
+			
+			req.session.loginUser = user.name;
 			res.json({ret_code: 0, ret_msg: '登录成功'});							
 		});
 	}else{
-		return res.json({ret_code: 1, ret_msg: '账号或密码错误'});
-	}
+		res.json({ret_code: 1, ret_msg: '账号或密码错误'});
+	}	
 });
 
 app.get('/logout', function(req, res, next){
 	// 备注：这里用的 session-file-store 在destroy 方法里，并没有销毁cookie
 	// 所以客户端的 cookie 还是存在，导致的问题 --> 退出登陆后，服务端检测到cookie
 	// 然后去查找对应的 session 文件，报错
-	// session-file-store 本身的bug
+	// session-file-store 本身的bug	
+
 	req.session.destroy(function(err) {
 		if(err){
 			res.json({ret_code: 2, ret_msg: '退出登录失败'});
 			return;
 		}
 		
+		// req.session.loginUser = null;
+		res.clearCookie(identityKey);
 		res.redirect('/');
 	});
 });
---
+++ examples/2016.12.06-session/users.js
@@ -0,0 +1,5 @@
+module.exports = {
+	items: [
+		{name: 'chyingp', password: '123456'}
+	]
+};

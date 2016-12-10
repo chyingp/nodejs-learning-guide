f24e1de4c934ce2b0cd07a6e6c75083fd2a7e9d0
casperchen(陈映平) <chyingp@gmail.com>
2016-12-10 16:25:53 +0800

    命令行工具

--- examples/2016.12.15-readline/commandLine/app.js
+++ examples/2016.12.15-readline/commandLine/app.js
@@ -1,11 +1,65 @@
 const readline = require('readline');
+const fs = require('fs');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'OHAI> '
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'OHAI> '
 });
 
-rl.prompt();
+const preHint = `
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See `npm help json` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg> --save` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.
+`;
+
+console.log(preHint);
+
+// 问题
+let questions = [ 'name', 'version', 'author'];
+
+// 默认答案
+let defaultAnswers = [ 'name', '1.0.0', 'none' ];
+
+// 用户答案
+let answers = [];
+let index = 0;
+
+function createPackageJson(){
+    var map = {};
+    questions.forEach(function(question, index){
+        map[question] = answers[index];
+    });
+
+    fs.writeFileSync('./package.json', JSON.stringify(map, null, 4));
+}
+
+function runQuestionLoop() {
+
+    if(index === questions.length) {
+        createPackageJson();
+        rl.close();
+        return;
+    }
+
+    var question;
+    var defaultAnswer;
+    
+    defaultAnswer = defaultAnswers.shift();
+    question = questions.shift() + '(' + defaultAnswer +')';
+    
+    rl.question(question, function(answer){
+        answers.push(answer || defaultAnswer);
+        index++;
+        runQuestionLoop();
+    });
+}
 
 rl.on('line', (line) => {
   switch(line.trim()) {

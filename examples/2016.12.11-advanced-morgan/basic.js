0ef23049816a9bb8def29f1fbef6441a32886400
casperchen(陈映平) <chyingp@gmail.com>
2016-12-11 17:04:25 +0800

    Morgan基础例子

---
+++ examples/2016.12.11-advanced-morgan/basic.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.use(morgan('short'));
+app.use(function(req, res, next){
+	res.send('ok');
+});
+
+app.listen(3000);

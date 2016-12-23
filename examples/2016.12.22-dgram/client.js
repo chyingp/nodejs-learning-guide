9aadf63c50a8ed6ca1843aa1c4be0bcda696faa9
casperchen(陈映平) <chyingp@gmail.com>
2016-12-23 15:27:52 +0800

    添加例子

---
+++ examples/2016.12.22-dgram/client.js
@@ -0,0 +1,12 @@
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var message = Buffer.from('My KungFu is Good!');
+
+var client = dgram.createSocket('udp4');
+client.send(message, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+    client.close();
+});
---
+++ examples/2016.12.22-dgram/server.js
@@ -0,0 +1,17 @@
+// 参考：http://www.hacksparrow.com/node-js-udp-server-and-client-example.html
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+});
+
+server.bind(PORT, HOST);

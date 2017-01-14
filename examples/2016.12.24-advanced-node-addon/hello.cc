9931e99c93e9a6aef9362c999f2b12645b144a1d
casperchen(陈映平) <chyingp@gmail.com>
2017-01-14 10:18:42 +0800

    add-on例子

--- examples/2016.12.24-advanced-node-addon/hello.cc
+++ examples/2016.12.24-advanced-node-addon/hello.cc
@@ -9,6 +9,21 @@ using v8::Object;
 using v8::String;
 using v8::Value;
 
+int32_t CheckSum(const char *buff, int64_t size) {
+    int32_t cksum = 0;
+    while (size > 1) {
+        cksum += *(unsigned short *) buff;
+        buff += 2;
+        size -= 2;
+    }
+    if (size) {
+        cksum += *buff;
+    }
+    while (cksum >> 16)
+        cksum = (cksum >> 16) + (cksum & 0xffff);
+    return ~cksum;
+}
+
 void Method(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "hello"));

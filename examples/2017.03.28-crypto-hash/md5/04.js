db53df9b0febb981f05111bfd4baa4a8a38ee237
casperchen(陈映平) <chyingp@gmail.com>
2017-03-28 12:57:29 +0800

    md5碰撞的例子

--- examples/2017.03.28-crypto-hash/md5/04.js
+++ examples/2017.03.28-crypto-hash/md5/04.js
@@ -1,13 +1,32 @@
-var str = `d131dd02c5e6eec4693d9a0698aff95c
-2fcab58712467eab4004583eb8fb7f89
-55ad340609f4b30283e488832571415a
-085125e8f7cdc99fd91dbdf280373c5b
-d8823e3156348f5bae6dacd436c919c6
-dd53e2b487da03fd02396306d248cda0
-e99f33420f577ee8ce54b67080a80d1e
-c69821bcb6a8839396f9652b6ff72a70`;
+function getHashResult(hexString){
 
-str = str.replace(/(\w{2,2})/g, '$1 ').replace(/ \n/g, '\n');
+	// 转成16进制，比如 0x4d 0xc9 ...
+	hexString = hexString.replace(/(\w{2,2})/g, '0x$1 ').trim();
 
-console.log(str);
+	// 转成16进制数组，如 [0x4d, 0xc9, ...]
+	var arr = hexString.split(' ');
 
+	// 转成对应的buffer，如：<Buffer 4d c9 ...>
+	var buff = Buffer.from(arr);
+
+	var crypto = require('crypto');
+	var hash = crypto.createHash('md5');
+
+	// 计算md5值
+	var result = hash.update(buff).digest('hex');
+
+	return result;	
+}
+
+// hexString = '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa200a8284bf36e8e4b55b35f427593d849676da0d1555d8360fb5f07fea2';
+var str1 = '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa200a8284bf36e8e4b55b35f427593d849676da0d1555d8360fb5f07fea2';
+var str2 = '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa202a8284bf36e8e4b55b35f427593d849676da0d1d55d8360fb5f07fea2';
+
+var result1 = getHashResult(str1);
+var result2 = getHashResult(str2);
+
+if(result1 === result2) {
+	console.log(`Got the same md5 result: ${result1}`);
+}else{
+	console.log(`Not the same md5 result`);
+}
